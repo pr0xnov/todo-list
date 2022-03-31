c4ba135db9816e25599020d96ecff1881d7a0752
pr0xnov <pr0xnov@gmail.com>
2022-03-31 21:37:22 +0300

    added all files

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useAppDispatch } from "../store/hooks";
+import { addTodo } from "../store/reducers/todosSlice";
+import style from "./todoForm.module.scss";
+
+function TodoForm() {
+  const dispatch = useAppDispatch();
+  const [text, setText] = useState("");
+
+  function changeHandler(e: React.FormEvent<HTMLInputElement>) {
+    setText(e.currentTarget.value);
+  }
+
+  function submitHandler(e: React.FormEvent) {
+    e.preventDefault();
+
+    dispatch(addTodo(text));
+    setText("");
+  }
+
+  return (
+    <form className={style["todo-form"]} onSubmit={submitHandler}>
+      <div className={style["todo-add"]}>
+        <input
+          onChange={changeHandler}
+          value={text}
+          placeholder="Add your new todo"
+        />
+        <button>
+          <div />
+          <div />
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default TodoForm;
